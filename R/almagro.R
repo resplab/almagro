827e7a94de92b2473961aaff3d531ec9693ed0fd
yzr96 <eric.yu@usask.ca>
2021-07-14 11:15:53 -0700

    add docstring

--- R/almagro.R
+++ R/almagro.R
@@ -1,5 +1,17 @@
+#' The CODEX index in COPD
+#'
+#' @param charlson_index The Charlson Index relates long-term mortality with the patient’s comorbidity. In general, no comorbidity is considered to be: 0-1 point, low comorbidity: 2 points, and high > 3 points.
+#' @param fev A number for forced expiratory volume in one second (FEV1) expressed as percentage
+#' @param mmrc A number for the dyspnea scale of modified Medical Research Council (mMRC)
+#' @param exacerbation Severe exacerbations of COPD during the previous year (hospitalization or ED consultations).
+#'
+#' @return the CODEX index
+#' @export
+#'
+#' @examples
+#' almagro(charlson_index=1, fev=65, mmrc=1, exacerbation=2)
 almagro <- function(charlson_index, fev, mmrc, exacerbation) {
-  if (charlson_index >= 0 & charlson_index <= 4){
+  if (charlson_index >= 0 & charlson_index <= 4) {
     comorbidity <- 0
   } else if (charlson_index >= 5 & charlson_index <= 7) {
     comorbidity <- 1
@@ -7,7 +19,7 @@ almagro <- function(charlson_index, fev, mmrc, exacerbation) {
     comorbidity <- 2
   }
 
-  if (fev >= 65){
+  if (fev >= 65) {
     obstruction <- 0
   } else if (fev >= 50 & fev <= 64) {
     obstruction <- 1
@@ -17,7 +29,7 @@ almagro <- function(charlson_index, fev, mmrc, exacerbation) {
     obstruction <- 3
   }
 
-  if (mmrc >= 0 & mmrc <= 1){
+  if (mmrc >= 0 & mmrc <= 1) {
     dyspnea <- 0
   } else if (mmrc == 2) {
     dyspnea <- 1
@@ -27,7 +39,7 @@ almagro <- function(charlson_index, fev, mmrc, exacerbation) {
     dyspnea <- 3
   }
 
-  if (exacerbation == 0 ){
+  if (exacerbation == 0) {
     ex <- 0
   } else if (exacerbation >= 1 & exacerbation <= 2) {
     ex <- 1
